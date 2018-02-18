7867e790ab20d84ef1094be738f3369bf0ca5759
MayorMonty <artificalchamber@gmail.com>
2018-02-17 22:39:31 -0500

    DRY: Make general purpose profile() function

--- lib/profile.c
+++ lib/profile.c
@@ -10,7 +10,7 @@
  * @param int target The target measured value
  * @return int progress The way that has
  **/
-float profileProgress(int start, int current, int target) {
+float profileProgress(int start, int target, int current) {
     return abs((float) current - (float) start) / abs((float) target - (float) start);
 }
 
@@ -26,7 +26,7 @@ float debugPosition;
  * @return int multipler A value from 0 to 1 to multiply your PWM by
  **/
 float profileTrapezoid(int startTicks, int targetTicks, int currentTicks, float dvPeriod = 1.0/3.0) {
-    float position = profileProgress(startTicks, currentTicks, targetTicks);
+    float position = profileProgress(startTicks, targetTicks, currentTicks);
     int accelerateUntil = (targetTicks * dvPeriod);
     int decelerateAt = targetTicks - (targetTicks * dvPeriod);
 
@@ -67,4 +67,29 @@ float profileJerk(int startTicks, int targetTicks, int currentTicks, float dvPer
 
     // In the strange occurance that none of the above conditions activate, eliminate the multiplier
     return 1;
+}
+
+#define PROFILE_TRAPEZOID 0
+#define PROFILE_JERK 1
+/**
+ * Makes a full motion profile calculation, with minimum and maximum PWM, and returns the correct PWM to set your motors to
+ * @param int profileType The profile to use, use PROFILE_TRAPEZOID, PROFILE_JERK, etc.
+ * @param int min The minimum PWM to ever set
+ * @param int max The maximum PWM to ever set
+ * @param startTicks The tick measure at the start of the movement
+ * @param targetTicks The total number of ticks to be achieved by the profile
+ * @param int currentTicks The current tick measures
+ * @param float dvPeriod The fraction of time for the acceleration and deceleration period
+ * @return int pwm
+ */
+int profile(int profileType, int min, int max, int startTicks, int targetTicks, int currentTicks, float dvPeriod = 1.0/3.0) {
+    float multipler;
+    switch(profileType) {
+        default: case PROFILE_TRAPEZOID:
+            multipler = profileTrapezoid(startTick, targetTicks, currentTicks, dvPeriod);
+        case PROFILE_JERK:
+            multipler = profileJerk(startTick, targetTicks, currentTicks, dvPeriod);
+
+    }
+    return (int)((max - min) * multipler + min);
 }
